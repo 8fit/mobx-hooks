b63b37aba33d925b3d8c78778397b2b062d69921
Kav Singh <flakisms@gmail.com>
2019-11-22 16:28:43 +0100

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,7 @@ module.exports = ({ env }) => ({
   plugins: [
     // For mobx
     ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-class-properties',
     // TODO: remove these when included with preset-env
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
