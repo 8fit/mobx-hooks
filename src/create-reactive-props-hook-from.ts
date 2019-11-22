137a2776adf7891413b0d57fd39edbe2d78528c0
Kav Singh <flakisms@gmail.com>
2019-11-22 22:02:52 +0100

    some cleanup

--- src/create-reactive-props-hook-from.ts
+++ src/create-reactive-props-hook-from.ts
@@ -1,6 +1,6 @@
 import { useMemo, useCallback } from 'react';
 
-import { Options, PropsMapWithSource } from './types';
+import { Options, PropsMapWithSource, PropsMap } from './types';
 import useReactiveProps from './use-reactive-props';
 
 // TODO: find a way to type internals properly [@kavsingh]
@@ -12,24 +12,24 @@ const attachSourceToPropsMap = <S, T>(
   Object.entries(propsMap).reduce((acc, [key, expression]: [unknown, any]) => {
     acc[key as keyof T] = () => expression(source);
     return acc;
-  }, {} as { [K in keyof T]: () => T[K] });
+  }, {} as PropsMap<T>);
 
 const createReactivePropsHookFrom = <S>(source: S) => <T>(
   propsMap: PropsMapWithSource<S, T>,
   options?: Options<T>,
 ) => {
-  const propsMapWithSource = useMemo(
+  const reactivePropsMap = useMemo(
     () => attachSourceToPropsMap(source, propsMap),
     [propsMap],
   );
-  const state = useReactiveProps(propsMapWithSource, options);
+  const state = useReactiveProps(reactivePropsMap, options);
   const withSource = useCallback(
-    <H>(handler: (source: S) => H) => handler(source),
+    <R>(handler: (source: S) => R) => handler(source),
     [],
   );
 
   return [state, withSource] as const;
 };
+/* eslint-enable */
 
 export default createReactivePropsHookFrom;
-/* eslint-enable */
